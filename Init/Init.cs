a88b98a3d15e2dce8f3c363151c7addeac323288
Huehtt1202@ <Hoangtthue0220@gmail.com>
2024-05-14 14:26:49 +0700

    login

---
+++ Init/Init.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutomateTestingLearningPlan.Init
+{
+    public class Init1
+    {
+        /*public IWebDriver driver;*/
+        public IWebDriver driver = new ChromeDriver();
+        [TestFixtureSetUp]
+        public void SetUpTest()
+        {
+            //Select browser
+            // Set timeouts for each step
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            // Set timeouts for page load
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+            // extend monitor
+            driver.Manage().Window.Maximize();
+        }
+        [TestFixtureTearDown]
+        public void TearDownTest()
+        {
+            Thread.Sleep(1000);
+            driver.Quit();
+        }
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,60 @@
+﻿using AutomateTestingLearningPlan.Init;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomateTestingLearningPlan
+{
+    [TestFixture]
+    public class Login : Init1
+    {
+        public class User
+        {
+            public string userName { get; set; }
+            public string password { get; set; }
+        }
+        public void FindElementOnLoginScreen()
+        {
+            User _user = new User();
+            //Go to login page
+            driver.Navigate().GoToUrl("https://app-v3.onluyen.vn/login");
+            //Check title of page is correct 
+            Console.Write(driver.Title);
+            //StringAssert.Contains(driver.Title, "Onluyen");
+            //Find element userName             
+            IWebElement _userName = driver.FindElement(By.CssSelector("[type='text'][placeholder='Tên đăng nhập hoặc số điện thoại']"));
+            _userName.SendKeys(_user.userName);
+            //find element password
+            IWebElement password = driver.FindElement(By.CssSelector("[type='password'][placeholder='Mật khẩu']"));
+            password.SendKeys(_user.password);
+            //find element btn_login
+            IWebElement _loginbtn = driver.FindElement(By.ClassName("btn-login"));
+            _loginbtn.Click();
+            //Find field remember or forgot
+            IWebElement field_remember_forgot = driver.FindElement(By.ClassName("field-remember-forgot"));
+            //find field remember login
+            IWebElement remember_login = field_remember_forgot.FindElement(By.ClassName("left-field"));
+            //find field forgot password
+            IWebElement forgot_password = field_remember_forgot.FindElement(By.ClassName("right-field"));
+
+        }
+        [Test]
+        void testLogin()
+        {
+            User teacher = new User();
+            teacher.userName = "huehtt34@ed.onluyen.vn";
+            teacher.password = "123123";
+            FindElementOnLoginScreen();
+            _loginbtn.Click();
+        }
+        /*[Test]*/
+        void testForgetPassword()
+        {
+            forgot_password.Click();
+        }
+    }
+}
