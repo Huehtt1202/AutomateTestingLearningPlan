cf966d00fe0e3c9fd42883a6048782b94496a2d3
Huehtt1202 <hoangtthue0220@gmail.com>
2024-05-28 17:37:38 +0700

    ref-out

--- Program.cs
+++ Program.cs
@@ -8,25 +8,23 @@ namespace AutomateTestingLearningPlan.Program
     {
         public static void Main(string[] args)
         {
-            /*Login login = new Login();*/
-            int countLoop = 0;
-            int countLoopTime = 0;
-            int valueNum = 10;
-            int loopTime = 5;
-            int minRandomValue = 0;
-            int maxRandomValue = 100;
-            Random random = new Random();
-            while (countLoopTime < loopTime)
-            {
-                countLoop = 0;
-                while (countLoop < valueNum)
-                {
-                    int showValue = random.Next(minRandomValue, maxRandomValue);
-                    Console.Write("{0.8}", showValue);
-                    countLoop++;
-                }
-            }
-            Console.ReadLine();
+            int a = 2; int b = 3;
+            CountRef(ref a, ref b);
+            int f, d, e;
+            CountOut(out f, out d);
+        }
+        static void CountRef( ref int a, ref int b)
+        {
+            a++;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+        static void CountOut(out int f, out int d)
+        {
+            f = 0; d = 3;
+            f++;
+            int e = f + d;
+            Console.WriteLine(e);
         }
     }
 }
