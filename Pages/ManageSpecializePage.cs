7ef9c7ee0d8a874461e3f7239308e0f8e240f7e9
Huehtt1202 <hoangtthue0220@gmail.com>
2024-07-12 16:59:47 +0700

    navitgate to function

--- Pages/ManageSpecializePage.cs
+++ Pages/ManageSpecializePage.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,26 +19,46 @@ namespace AutomateTestingLearningPlan.Pages
             this.driver = driver;
         }
         /// <summary>
-        /// Access to a function of the manage specialize feature which is selected by principal role
+        /// Access to a function of the manage specialize feature which is selected by Account has one role
         /// </summary>
-        /// <param name="functionTitle">Enter title need to navigate</param>
-        public void NavigateToFuncOfMSWithMasterRole(string functionTitle)
+        public void NavigateToEachFuncOfMS()
         {
+            string urlMS = driver.Url;
             //IWebElement FunctionsOfMaster = _act.GetElement(By.ClassName("manage-leader ng-star-inserted"));
-            IWebElement FunctionsOfMaster = _act.GetElement(By.CssSelector("div.manage-specialize div:nth-of-type(1)"));
-            IList<IWebElement> listFunction = FunctionsOfMaster.FindElements(By.CssSelector("div.list-function>div"));
-            for(int i = 0; i < listFunction.Count; i++)
+            IWebElement contents = _act.GetElement(By.ClassName("manage-specialize"));
+            IList<IWebElement> listFunction = contents.FindElements(By.CssSelector("div.list-function>div"));
+            foreach(var func in listFunction)
             {
-                IWebElement titleFunction = listFunction[i];
-                if (functionTitle.Equals(titleFunction.Text))
+                func.Click();
+                string url = driver.Url;
+                if (url != urlMS)
                 {
-                    titleFunction.Click();
+                    driver.Navigate().Back();
+                }
+                else Console.WriteLine("function died: + {func}");
+            }
+        }
+        /// <summary>
+        /// Access to a function of the manage specialize feature which is selected by Account has multiple role
+        /// </summary>
+        /// <param name="functionTitle"></param>
+        public void NavigateToFuncOf(string trigger)
+        {
+            IWebElement FunctionsOfMaster = _act.GetElement(By.CssSelector("div.manage-specialize div:nth-of-type(2)"));
+            IList<IWebElement> listFunction = FunctionsOfMaster.FindElements(By.CssSelector("div.list-function>div"));
+            for (int i = 0; i < listFunction.Count; i++)
+            {
+                IWebElement function = listFunction[i];
+                string[] titleFunctionGetAttribute;
+                if (functionTitle.Equals(function.GetCssValue("p")))
+                {
+                    function.Click();
                     return;
                 }
             }
         }
         /// <summary>
-        /// Access to a function of the manage specialize feature which is selected by leader role
+        /// Access to a function of the manage specialize feature which is selected by Account has multiple role
         /// </summary>
         /// <param name="functionTitle">Enter title need to navigate</param>
         public void NavigateToFuncOfMSWithLeaderRole1(string functionTitle)
